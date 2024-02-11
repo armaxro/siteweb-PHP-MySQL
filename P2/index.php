7cf8683f729bf3cdbb8e0385ba0d19e424bfe69d
Maxi Rojas <arnaldomrojas@outlook.com>
2024-02-11 10:34:39 +0100

    php openclassroom

--- P2/index.php
+++ P2/index.php
@@ -1,2 +1,34 @@
-<?php
-phpinfo();
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Ceci est une page HTML de test</title>
+    </head>
+
+    <body>
+        <h2>Page de test</h2>
+        <p>
+            Cette page contient <strong>uniquement</strong> du code HTML.<br/>
+            
+
+            Voici quelques petits tests : <br>
+                         
+            <?php
+            $age = 22;
+
+            if ($age >18){
+                echo "Bienvenue";
+            }else{
+                echo "Vous etes mineur";
+            }
+    
+            ?>
+
+        </p>
+
+        <ul>
+            <li style="color: blue;">Texte en bleu</li>
+            <li style="color: red;">Texte en rouge</li>
+            <li style="color: green;">Texte en vert</li>
+        </ul>
+    </body>
+</html>
